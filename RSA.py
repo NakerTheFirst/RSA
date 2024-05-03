621712a4071259f8c5ea7ba0f48f408b159bbc70
Krzysztof <kcholysk@gmail.com>
2024-05-03 17:57:27 +0200

    Add GCD function

--- RSA.py
+++ RSA.py
@@ -9,12 +9,37 @@ def generate_prime():
     return n
 
 
+def gcd(a, b):
+    if a == 0:
+        return b
+
+    if b == 0:
+        return a
+
+    # Swap the values so `a` is always bigger
+    if a < b:
+        a, b = b, a
+
+    remainder = a % b
+    return gcd(b, remainder)
+
+
 def main():
 
     p, q = generate_prime(), generate_prime()
 
     print(f"p: {p}")
     print(f"q: {q}")
+
+    modulus = p * q
+
+    euler_totient = (p - 1) * (q - 1)
+
+    print(euler_totient)
+
+    pub_exp = 65537
+
+    print(gcd(270, 192))
 
     return 0
 

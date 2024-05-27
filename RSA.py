771bccd6aeff0922e99b8ad88af37af213f30660
Krzysztof <kcholysk@gmail.com>
2024-05-27 19:50:24 +0200

    Fix incorrect modular inverse calculation

--- RSA.py
+++ RSA.py
@@ -26,29 +26,47 @@ def extended_gcd(a, b):
         return gcd, x, y
 
 
+def get_mod_inv(a, b):
+    gcd, x, y = extended_gcd(a, b)
+
+    if gcd != 1:
+        raise ValueError(f"Modular inverse does not exist for {a} modulo {b}")
+
+    if x < 0:
+        x += b
+
+    return x
+
+
 def is_coprime(a, b):
     return gcd(a, b) == 1
 
 
 def main():
 
+    text_to_encrypt = "Lorem"
     p, q = generate_prime(), generate_prime()
 
-    print(f"p: {p}")
-    print(f"q: {q}")
-
     modulus = p * q
-
     euler_totient = (p - 1) * (q - 1)
-
-    print(euler_totient)
-
-    pub_exp = 65537
+    pub_exp = 17
 
     if not is_coprime(modulus, pub_exp):
         raise Exception(f"Modulus: {modulus} is not coprime to {pub_exp}")
 
-    d = extended_gcd(modulus, pub_exp)
+    priv_exp = get_mod_inv(modulus, pub_exp)
+
+    public_key = (pub_exp, modulus)
+    private_key = (priv_exp, modulus)
+
+    print(modulus)
+
+    m = 2137
+    c = m**pub_exp % modulus
+
+    og_message = c**priv_exp % modulus
+
+    print(og_message)
 
     return 0
 
